bca3b289f6f66205b9c656b5173e1c38826fb061
Keith <keithtmccartney@hotmail.com>
2020-09-29 12:14:24 +0100

    Applying CORS startup changes, applying README changes.

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ Grab the article at [https://code-maze.com/netcore-signalr-angular/](https://cod
 
 * Abc
 
+## Notes
+
+* The lack of use of 'AllowAnyOrigin' (in favour of 'WithOrigins') on the Server app when enabling CORS (Cross-origin resource sharing) is because in .NET Core 3.0 the combination of AllowAnyOrigin and AllowCredentials is considered an insecure CORS configuration; a detailed guide has been provided at [https://code-maze.com/enabling-cors-in-asp-net-core/](https://code-maze.com/enabling-cors-in-asp-net-core/)
+
 ## What's it all about?
 
 * In this article, we are going to show you how to use SignalR with .NET Core and Angular through the practical example
--- urban-disco/RealTimeCharts_Server/Startup.cs
+++ urban-disco/RealTimeCharts_Server/Startup.cs
@@ -16,6 +16,16 @@ namespace RealTimeCharts_Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder
+                .WithOrigins("http://localhost:4200")
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+            });
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +36,22 @@ namespace RealTimeCharts_Server
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+
+                endpoints.MapControllers();
             });
         }
     }
